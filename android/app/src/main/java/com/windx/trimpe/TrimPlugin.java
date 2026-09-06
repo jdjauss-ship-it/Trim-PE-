610d845bf96bda25db783aa58e73772555bbab4b
WindX <jdjauss@gmail.com>
2026-09-06 16:56:49 +0600

    Remove unused PluginMethod annotation

--- android/app/src/main/java/com/windx/trimpe/TrimPlugin.java
+++ android/app/src/main/java/com/windx/trimpe/TrimPlugin.java
@@ -4,12 +4,10 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
 
 @CapacitorPlugin(name = "TrimPlugin")
 public class TrimPlugin extends Plugin {
 
-    @PluginMethod
     public void testConnection(PluginCall call) {
 
         JSObject result = new JSObject();
