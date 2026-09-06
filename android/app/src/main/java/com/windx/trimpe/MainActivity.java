b0827fd315f3e731e82d244d3360b9a5ddb7b09b
WindX <jdjauss@gmail.com>
2026-09-06 16:45:11 +0600

    Register TrimPlugin in MainActivity
    Override onCreate method to register TrimPlugin.

--- android/app/src/main/java/com/windx/trimpe/MainActivity.java
+++ android/app/src/main/java/com/windx/trimpe/MainActivity.java
@@ -1,5 +1,14 @@
 package com.windx.trimpe;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        registerPlugin(TrimPlugin.class);
+    }
+}
