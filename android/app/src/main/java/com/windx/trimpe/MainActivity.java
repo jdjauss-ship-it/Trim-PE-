6b80f3caa2dc9494e6b1e4016f2520dbcf8b7328
WindX <jdjauss@gmail.com>
2026-09-06 19:03:00 +0600

    Move super.onCreate call after plugin registration

--- android/app/src/main/java/com/windx/trimpe/MainActivity.java
+++ android/app/src/main/java/com/windx/trimpe/MainActivity.java
@@ -1,14 +1,16 @@
 package com.windx.trimpe;
 
 import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
         registerPlugin(TrimPlugin.class);
+
+        super.onCreate(savedInstanceState);
     }
 }
