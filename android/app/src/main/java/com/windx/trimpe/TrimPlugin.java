8b0058c2f1af1f5be0ed96a9e2872da75b4cab32
WindX <jdjauss@gmail.com>
2026-09-06 18:39:37 +0600

    Add testConnection method to TrimPlugin

--- android/app/src/main/java/com/windx/trimpe/TrimPlugin.java
+++ android/app/src/main/java/com/windx/trimpe/TrimPlugin.java
@@ -3,11 +3,13 @@ package com.windx.trimpe;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "TrimPlugin")
 public class TrimPlugin extends Plugin {
 
+    @PluginMethod
     public void testConnection(PluginCall call) {
 
         JSObject result = new JSObject();
