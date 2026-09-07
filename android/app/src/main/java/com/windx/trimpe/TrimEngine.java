4795c2f624d600fd688bc4fce5b6ba1760bf16db
WindX <jdjauss@gmail.com>
2026-09-07 17:10:15 +0600

    Add native method prepareTrim to TrimEngine

--- android/app/src/main/java/com/windx/trimpe/TrimEngine.java
+++ android/app/src/main/java/com/windx/trimpe/TrimEngine.java
@@ -7,4 +7,12 @@ public class TrimEngine {
     }
 
     public native String testNativeEngine();
+
+    public native String prepareTrim(
+            String worldPath,
+            int x1,
+            int z1,
+            int x2,
+            int z2
+    );
 }
